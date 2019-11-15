a68a2febfc62f786daa2d89f102a18e0fe9a29a5
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-11-15 18:11:43 +0200

    add js and modal

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
- let a = document.querySelectorAll("a");
-document.addEventListener('click', function (e) {
- e.preventDefault()
+let a = document.querySelectorAll('a');
+document.addEventListener('click', function(e) {
+  //  e.preventDefault()
 });
 console.log(a);
-for (let i = 0; i < a.length; i++) {
- 
-}
+
+let overlay = document.querySelector('.overlay');
+let modal = document.querySelector('.features_title3');
+console.log(modal);
+let login = document.querySelector('[href="#login"]');
+login.addEventListener('click', function(e) {
+  overlay.classList.add('show');
+  modal.classList.add('show');
+  overlay.addEventListener('click', function(e) {
+    overlay.classList.remove('show');
+    modal.classList.remove('show');
+  });
+});
+
+setTimeout(function() {
+  overlay.classList.add('show');
+  modal.classList.add('show');
+  overlay.addEventListener('click', function(e) {
+    overlay.classList.remove('show');
+    modal.classList.remove('show');
+  });
+}, 2000);
