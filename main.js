57be233e4dbd63595b8b6c6fcf492d49a370ccb7
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-11-16 10:24:32 +0200

    1

--- main.js
+++ main.js
@@ -1,24 +1,17 @@
-
-
-
 $('.a').on('click', function(e) {
-   e.preventDefault();
-   
+  e.preventDefault();
+
   var atr = $(this).attr('href');
 
   var position = $(atr).offset().top;
-  
 
   $('html,body').animate({ scrollTop: position }, 1000);
-
 });
 
 // $(document).on('click', function () {
 //   console.log(1);
 //   $(document).scrollTop(500);
 // });
- 
-
 
 let overlay = document.querySelector('.overlay');
 let modal = document.querySelector('.features_title3');
@@ -27,17 +20,21 @@ let login = document.querySelector('[href="#login"]');
 login.addEventListener('click', function(e) {
   overlay.classList.add('show');
   modal.classList.add('show');
+
   overlay.addEventListener('click', function(e) {
-    overlay.classList.remove('show');
-    modal.classList.remove('show');
+    console.log(e.target.className == 'overlay show');
+    if (e.target.className == 'overlay show') {
+       overlay.classList.remove('show');
+       modal.classList.remove('show');
+     }
   });
 });
 
-setTimeout(function() {
-  overlay.classList.add('show');
-  modal.classList.add('show');
-  overlay.addEventListener('click', function(e) {
-    overlay.classList.remove('show');
-    modal.classList.remove('show');
-  });
-}, 2000);
+// setTimeout(function() {
+//   overlay.classList.add('show');
+//   modal.classList.add('show');
+//   overlay.addEventListener('click', function(e) {
+//     overlay.classList.remove('show');
+//     modal.classList.remove('show');
+//   });
+// }, 2000);
