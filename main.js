9d5965c4ec965b9f39c058c69f206780dea5f1d4
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-11-23 17:34:27 +0200

    1

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ $('.a').on('click', function(e) {
 
   var atr = $(this).attr('href');
 
-  var position = $(atr).offset().top;
+  var positionElement = $(atr).offset().top;
 
-  $('html,body').animate({ scrollTop: position }, 1000);
+  $('html,body').animate({ scrollTop: positionElement }, 1000);
 });
 
 // $(document).on('click', function () {
@@ -16,8 +16,11 @@ $('.a').on('click', function(e) {
 let overlay = document.querySelector('.overlay');
 let modal = document.querySelector('.features_title3');
 
-let login = document.querySelector('[href="#login"]');
-login.addEventListener('click', function(e) {
+let login = document.querySelectorAll('[href="#login"]');
+//получаем    все логины их 2
+
+// вешаем обработчик на логин на который кликнули
+login[0].addEventListener('click', e => {
   overlay.classList.add('show');
   modal.classList.add('show');
   overlay.addEventListener(
@@ -33,28 +36,28 @@ login.addEventListener('click', function(e) {
         div.style.left = coordLeft + 'px';
         div.textContent = 'close';
         document.body.before(div);
+        console.log(div);
       }
     },
     { once: true },
   );
-
-  document.addEventListener('click', function(e) {
-    console.log(e.target);
-    if (e.target.className == 'span' || e.target.className == 'overlay show') {
-      overlay.classList.remove('show');
-      modal.classList.remove('show');
-      document.querySelector('.span').remove();
-    }
-  });
 });
 
+document.addEventListener('click', function(e) {
+  // console.log(e.target);
+  if (e.target.className == 'span' || e.target.className == 'overlay show') {
+    overlay.classList.remove('show');
+    modal.classList.remove('show');
+    let span = document.querySelector('.span').remove();
+   
+  }
+});
 let arrimg = [
   './img/heder_fon1.png',
   './img/heder_fon2.png',
   './img/heder_fon4.png',
   './img/heder_fon5.png',
 ];
-
 
 let bg = document.querySelector('.header-section-bg');
 let button = document.querySelector('.btn--active');
