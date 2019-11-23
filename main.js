06401ab28e95be75932e0530c39b0715468fd561
SergeyBerezov <sergeyberezovsky1981@gmail.com>
2019-11-23 17:52:38 +0200

    1

--- main.js
+++ main.js
@@ -16,42 +16,46 @@ $('.a').on('click', function(e) {
 let overlay = document.querySelector('.overlay');
 let modal = document.querySelector('.features_title3');
 
-let login = document.querySelectorAll('[href="#login"]');
+let logins = document.querySelectorAll('[href="#login"]');
 //получаем    все логины их 2
+for (const element of logins) {
+  // вешаем обработчик на логин на который кликнули
+  element.addEventListener('click', e => {
+    overlay.classList.add('show');
+    modal.classList.add('show');
 
-// вешаем обработчик на логин на который кликнули
-login[0].addEventListener('click', e => {
-  overlay.classList.add('show');
-  modal.classList.add('show');
-  overlay.addEventListener(
-    'mouseout',
-    function(e) {
-      if (e.target.className == 'overlay show') {
-        let coordTop = e.pageY;
-        let coordLeft = e.pageX;
+    // вешаем обработчик на наше модальное окно  показываем сообщение можно закрыть окно
+    overlay.addEventListener(
+      'mouseout',
+      function(e) {
+        if (e.target.className == 'overlay show') {
+          let coordTop = e.pageY;
+          let coordLeft = e.pageX;
 
-        let div = document.createElement('span');
-        div.classList.add('span');
-        div.style.top = coordTop + 'px';
-        div.style.left = coordLeft + 'px';
-        div.textContent = 'close';
-        document.body.before(div);
-        console.log(div);
-      }
-    },
-    { once: true },
-  );
-});
-
+          let div = document.createElement('span');
+          div.classList.add('span');
+          div.style.top = coordTop + 'px';
+          div.style.left = coordLeft + 'px';
+          div.textContent = 'click to close';
+          document.body.before(div);
+          // console.log(div);
+        }
+      },
+      { once: true },
+    );
+  });
+}
+// клик обработчик закрываем модальное окно
 document.addEventListener('click', function(e) {
   // console.log(e.target);
   if (e.target.className == 'span' || e.target.className == 'overlay show') {
     overlay.classList.remove('show');
     modal.classList.remove('show');
-    let span = document.querySelector('.span').remove();
-   
+    document.querySelector('.span').remove();
   }
 });
+
+// получаем наши катринки в делаем простой слайд
 let arrimg = [
   './img/heder_fon1.png',
   './img/heder_fon2.png',
